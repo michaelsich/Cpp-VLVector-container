05d05c057573cd1b4c3c5621a17c83968a63bc5a
michaelsich <micha741@gmail.com>
2020-08-06 17:27:56 +0300

    switched to default iterators for sanity testing

--- VLVector.hpp
+++ VLVector.hpp
@@ -6,53 +6,73 @@
 #define CPPEXAM_VLVECTOR_HPP
 
 #include <cstddef>
-#include <iterator>
+#include <stdexcept>
 #include <cmath>
+#include <iterator> //TODO: delete~~~~!!!!
+#include <vector>
 
 #define DEFAULT_STATIC_CAPACITY     16
-#define CAP_C_FACTOR                (3 / 2)
+#define CAP_C_FACTOR                (3.0 / 2.0)
+#define DEFAULT_ADDED_ELEMENTS      1
+
+#define ERR_OUT_OF_RANGE            "Out of range"
+#define ERR_INVALID_POP             "No elements to pop"
 
 template<class T, size_t StaticCapacity = DEFAULT_STATIC_CAPACITY>
 class VLVector
 {
 public:
-    typedef typename VLVector<T>::VecIter       iterator;
-    typedef typename VLVector<T>::ConstVecIter  const_iterator;
+    typedef typename std::vector<T>::iterator       iterator;
+    typedef typename std::vector<T>::const_iterator const_iterator;
 
-    VLVector() : mSize(0), mCapacity(0) {*mainArr = {};};
+    VLVector() : mSize(0), mCapacity(StaticCapacity), mainArr(stackArr), mArrType(STATIC) {};
     VLVector(const VLVector<T>& vecToCopy);
     template<class InputIterator>
     VLVector(InputIterator& first, InputIterator& last);
     ~VLVector();
 
+    iterator begin();
 
+    inline size_t  size()      const noexcept   {return mSize;}
+    inline size_t  capacity()  const noexcept   {return mCapacity;}
+    inline bool    empty()     const noexcept   {return (mSize == 0);}
+    inline T*      data()      const noexcept   {return mainArr;}
 
-    inline size_t  size()      const noexcept {return mSize;}
-    inline size_t  capacity()  const noexcept {return mCapacity;}
-    inline bool    empty()     const noexcept;
-
-    void    push_back(T);
+    T&      at(size_t i);
+    void    push_back(const T& element);
 
 private:
+    enum ArrType
+    {
+        DYNAMIC,
+        STATIC
+    };
+
+    //region class members
     size_t  mSize;
     size_t  mCapacity;
     T*      mainArr;
+    T       stackArr[StaticCapacity];
+    ArrType mArrType;
+    //endregion
 
-    size_t  _capC(const size_t& newElements) const;
+    size_t  _capC            (const size_t& newElements)    const;
+    void    _enlargeCapacity (const size_t &newElements);
+    void    _copyAllElements (T* destination)               const;
 
     //region class Iterators
-    class VecIter
+    class Iterator
     {
     public:
         /* iter traits */
         typedef T           value_type;
         typedef T&          reference;
         typedef T*          pointer;
-        typedef ptrdiff_t   difference_type;    //TODO: verify allowed to use this
+        typedef ptrdiff_t   difference_type;
         typedef std::random_access_iterator_tag iterator_category;
 
         /* read / write */
-        VecIter(pointer ptr = nullptr) : currIter(ptr) {};
+        Iterator(pointer ptr = nullptr) : currIter(ptr) {};
         inline  reference operator*() const     {return *currIter;} // de-reference iter
 
         /* iteration */
@@ -100,9 +120,9 @@ private:
         typedef T           value_type;
         typedef T&          reference;
         typedef T*          pointer;
-        typedef ptrdiff_t   difference_type;    //TODO: verify allowed to use this
+        typedef ptrdiff_t   difference_type;
         typedef std::random_access_iterator_tag iterator_category;
-        
+
         /* read / write */
         ConstVecIter(pointer ptr = nullptr) : currIter(ptr) {};
         inline  value_type operator*() const     {return *currIter;} // de-reference iter
@@ -160,15 +180,19 @@ VLVector<T, StaticCapacity>::VLVector(const VLVector<T> &vecToCopy)
 {
     mSize = vecToCopy.mSize;
     mCapacity = vecToCopy.mCapacity;
+    mArrType = vecToCopy.mArrType;
 
-    if (mCapacity > StaticCapacity)
-    {   //static array
-        *mainArr = {};
+    switch (mArrType)
+    {
+        case STATIC:
+            mainArr = stackArr;
+            break;
+
+        case DYNAMIC:
+            mainArr = new T[mCapacity];
+            break;
     }
-    else
-    {   // dynamic arr
-        mainArr = new T[mCapacity];  // TODO: should I add nothrow?
-    }
+
     for (size_t i = 0; i < mSize; ++i)
     {
         mainArr[i] = vecToCopy.mainArr[i];
@@ -189,6 +213,29 @@ VLVector<T, StaticCapacity>::~VLVector()
     }
 }
 
+template<class T, size_t StaticCapacity>
+T &VLVector<T, StaticCapacity>::at(size_t i)
+{
+    if (i >= mSize || i < 0)
+    {
+        throw std::out_of_range(ERR_OUT_OF_RANGE);
+    }
+    return mainArr[i];
+}
+
+
+
+template<class T, size_t StaticCapacity>
+void VLVector<T, StaticCapacity>::push_back(const T &element)
+{
+    if (mSize >= mCapacity)
+    {
+        _enlargeCapacity(DEFAULT_ADDED_ELEMENTS);
+    }
+    mainArr[mSize++] = element;
+}
+
+//region private methods
 /**
  * @brief calculates the best capacity for VLVector
  * @param newElements num of elements of T planned to be added
@@ -206,6 +253,37 @@ size_t VLVector<T, StaticCapacity>::_capC(const size_t &newElements) const
         return std::floor((CAP_C_FACTOR) * (mSize + newElements) );
     }
 }
+
+template<class T, size_t StaticCapacity>
+void VLVector<T, StaticCapacity>::_enlargeCapacity(const size_t &newElements)
+{
+    size_t  newCap = _capC(newElements);
+    T*      newArr = new T[newCap];
+
+    _copyAllElements(newArr);
+    if (mArrType == DYNAMIC)
+    {
+        delete[] mainArr;
+    }
+    mArrType = DYNAMIC;
+    mainArr = newArr;
+    mCapacity = newCap;
+}
+
+template<class T, size_t StaticCapacity>
+void VLVector<T, StaticCapacity>::_copyAllElements(T *destination) const
+{
+    if (destination)
+    {
+        for (int i = 0; i < mSize; ++i)
+        {
+            destination[i] = mainArr[i];
+        }
+    }
+}
+
+
+//endregion
 
 
 #endif //CPPEXAM_VLVECTOR_HPP
--- main.cpp
+++ main.cpp
@@ -3,6 +3,26 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
+    VLVector<int,2> myVec;
+    myVec.push_back(1);
+    myVec.push_back(2);
+    myVec.push_back(3);
+    myVec.push_back(4);
+    myVec.push_back(5);
+    myVec.push_back(6);
+    myVec.push_back(7);
+
+    std::cout << "size: " << myVec.size() << std::endl;
+    std::cout << "capacity: " << myVec.capacity() << std::endl << std::endl;
+    int * data = myVec.data();
+    std::cout << "{ ";
+    for (int i = 0; i < 3; ++i)
+    {
+        std::cout << data[i] << ", ";
+    }
+    std::cout << " }" << std::endl;
+
+
     return 0;
 }
